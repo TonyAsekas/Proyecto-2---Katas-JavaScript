112d09b3c1e9055c913605a153550ad7542dc342
Antonio Alvarez Fernández <toni12tineo@gmail.com>
2026-07-26 12:18:22 +0200

    Add files via upload

---
+++ Proyecto 2 - Katas JavaScript/ejercicios-basicos-avanzados/ejercicio7.js
@@ -0,0 +1,13 @@
+function greaterNumber(numberOne , numberTwo) {
+  // Completar código
+  if(numberOne<numberTwo){
+    console.log(numberTwo)
+  }else if(numberOne>numberTwo){
+    console.log(numberOne)
+  }else{
+    console.log("Los números son iguales, dame otra variante distinta")
+  }
+}
+greaterNumber(5,8)
+greaterNumber(2.56, 1.45)
+greaterNumber(2,2)
