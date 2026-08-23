5f2b78a99e3725f633310cb57607d480d1350072
Antonio <toni12tineo@gmail.com>
2026-08-23 13:27:27 +0200

    actu

---
+++ ejercicios-DOM/ejercicio-02/script.js
@@ -0,0 +1,11 @@
+//2.1
+const newDiv = document.createElement("div")
+document.body.appendChild(newDiv)
+
+//2.2
+const newDiv2 = document.createElement("div")
+const newP = document.createElement("p")
+newDiv2.appendChild(newP)
+document.body.appendChild(newDiv2)
+
+//2.3
