0c2b20627645cf8c0ec2d5b4dfc3b497be69eaeb
Antonio <toni12tineo@gmail.com>
2026-08-23 13:15:27 +0200

    ejercicio-1

---
+++ ejercicios-DOM/ejercicio-01/script.js
@@ -0,0 +1,19 @@
+//1.1
+console.log(document.querySelector(".showme"))
+
+//1.2
+console.log(document.querySelector("#pillado"))
+
+//1.3
+console.log(document.querySelectorAll("p"))
+
+//1.4
+console.log(document.querySelectorAll(".pokemon"))
+
+//1.5
+// El primer span (posición 0):
+console.log(document.querySelectorAll("[data-function='testMe']"))
+
+//1.6
+// El primer span (posición 0):
+console.log(document.querySelectorAll("[data-function='testMe']")[2].textContent)
